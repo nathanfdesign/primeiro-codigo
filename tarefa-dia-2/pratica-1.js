08f425cb430d46fec7d8e2b4e88fd22b0303ab5e
Nathanael Franklin <nathanfdesign@gmail.com>
2022-08-24 13:53:39 -0300

    info

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -2,3 +2,7 @@ let nome = "Nathanael Franklin"
 let idade = 19
 let email = "nathanfdesign@gmail.com"
 let localFavorito = "Espanha"
+console.log(nome)
+console.log(idade)
+console.log(email)
+console.log(localFavorito)
