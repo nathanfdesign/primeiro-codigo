a90d8bef6b3f9336fb223fa351729e449c4d79a7
Nathanael Franklin <nathanfdesign@gmail.com>
2022-08-25 14:21:02 -0300

    Msg

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,8 +3,11 @@ let idade = 19
 let email = "nathanfdesign@gmail.com"
 let localFavorito = "Espanha"
 let profissao = "Designer"
-console.log(nome)
-console.log(localFavorito)
-console.log(idade)
-console.log(profissao)
-console.log(email)
+//console.log(nome)
+//console.log(localFavorito)
+//console.log(idade)
+//console.log(profissao)
+//console.log(email)
+let msg = "Olá, "+ nome +" ! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "+ localFavorito +". Confirma pra gente alguns dados? A sua idade é "+ idade +", você é "+ profissao +" e o seu email é "+ email +" Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+console.log(msg)
